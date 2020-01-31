7c58d28a7e44278bbd7f68f5b538f17c7c1b26a4
KCVO1995 <943514589@qq.com>
2020-01-31 18:39:15 +0800

    html ok

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 var path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
         filename: '[name].[contenthash].js'
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'My App',
+        })
+    ]
 };
