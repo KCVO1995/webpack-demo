2b166f8a0168f76ca453517ec049345021aae23e
KCVO1995 <943514589@qq.com>
2020-01-31 20:19:39 +0800

    css load

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import x from './x.js'
-
+import './style.css'
 console.log(x)
 console.log('hi')
--- webpack.config.js
+++ webpack.config.js
@@ -5,12 +5,28 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
+    devtool: 'inline-source-map',
     output: {
         filename: '[name].[contenthash].js'
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'My App',
+            title: 'jacky',
+            template: 'src/assets/admin.html'
         })
-    ]
+    ],
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [
+               'style-loader',
+               'css-loader'
+            ]
+        }]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 8080
+    }
 };
