dd11e297681bda7e0f26969c3f223ff14b6fc78f
KCVO1995 <943514589@qq.com>
2020-02-01 20:22:33 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 yarn build &&
 git checkout gh-pages &&
-mv dist/* ./ && 
-git add . && 
-git commit -m 'update' && 
-git push && 
-git checkout -
+rm -rf *.html *.js *.css *.png &&
+mv dist/* ./ &&
+rm -rf dist;
+git add . &&
+git commit -m 'update' &&
+git push
